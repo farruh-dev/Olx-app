8b9e2723065d31b6ee8e89340f14dab69489f0dd
Farrukh Olimov <farruhbekolimov3904@gmail.com>
2021-09-23 23:42:48 +0500

    add styles

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express")
 require("dotenv").config()
+const path = require("path")
 
 const app = express()
 const PORT = process.env.PORT || 5656;
+
+app.use(express.static(path.join(__dirname, "public")))
 
 
 app.listen(PORT, () => {
